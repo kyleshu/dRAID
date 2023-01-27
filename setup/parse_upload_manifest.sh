7472c2a006255201ef954f94b7016291e2ada4ef
kyleshu <shujunyi@gmail.com>
2023-01-27 16:01:06 +0800

    Update parse_upload_manifest.sh

--- setup/parse_upload_manifest.sh
+++ setup/parse_upload_manifest.sh
@@ -4,9 +4,9 @@ manifest=$1
 username=$2
 
 # generate host names and ip addresses from manifest
-line = `python3 parse_manifest.py ${manifest} hosts.txt ip_addrs_100g.txt ip_addrs_25g.txt`
+line=`python3 parse_manifest.py ${manifest} hosts.txt ip_addrs_100g.txt ip_addrs_25g.txt`
 
-echo "scp manifests to line"
+echo "scp manifests to $line"
 ssh -tt "$username@$line" "mkdir ~/artifacts"
 scp hosts.txt "$username@$line:~/artifacts/"
 scp ip_addrs_100g.txt "$username@$line:~/artifacts/"
