4e17cb091632cfdc936bb0530dd5a5c295efee30
kyleshu <shujunyi@gmail.com>
2023-01-28 19:24:45 +0800

    linux fig9

---
+++ FIO/Linux/fig9/mount.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo mdadm --create -v /dev/md0 --assume-clean --chunk=512 --level=5 --raid-devices=6 --size=134217728 /dev/nvme2n1 /dev/nvme3n1 /dev/nvme4n1 /dev/nvme5n1 /dev/nvme6n1 /dev/nvme7n1
+sudo mkfs -t ext4 /dev/md0
+sudo mount /dev/md0 /raid
+sudo mkdir /raid/fiotest
