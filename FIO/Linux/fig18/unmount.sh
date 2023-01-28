a62efe0f7e1796864085aa298bb044084525ac8e
kyleshu <shujunyi@gmail.com>
2023-01-28 21:54:30 +0800

    linux 18

---
+++ FIO/Linux/fig18/unmount.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo rm -r /raid/fiotest
+sudo umount /raid
+sudo mdadm -S /dev/md0
+
+sudo mdadm --zero-superblock /dev/nvme2n1
+sudo mdadm --zero-superblock /dev/nvme3n1
+sudo mdadm --zero-superblock /dev/nvme4n1
+sudo mdadm --zero-superblock /dev/nvme5n1
+sudo mdadm --zero-superblock /dev/nvme6n1
+sudo mdadm --zero-superblock /dev/nvme7n1
+sudo mdadm --zero-superblock /dev/nvme8n1
+sudo mdadm --zero-superblock /dev/nvme9n1
