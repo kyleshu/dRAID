4bd5e63fe6b88950faf9e4e123aaae558a710342
kyleshu <shujunyi@gmail.com>
2023-01-28 23:21:36 +0800

    Update generate_raid_config.sh

--- dRAID/scripts/generate_raid_config.sh
+++ dRAID/scripts/generate_raid_config.sh
@@ -8,7 +8,7 @@ python3 create_raid_config.py $HOME/artifacts/ip_addrs_100g.txt $HOME/artifacts/
 python3 create_raid_config.py $HOME/artifacts/ip_addrs_100g.txt $HOME/artifacts/raid6_100g.json $chunk 6 $raid_size $num_qp false
 python3 create_raid_config.py $HOME/artifacts/ip_addrs_25g.txt $HOME/artifacts/raid5_25g.json $chunk 5 $raid_size $num_qp false
 python3 create_raid_config.py $HOME/artifacts/ip_addrs_25g.txt $HOME/artifacts/raid6_25g.json $chunk 6 $raid_size $num_qp false
-python3 create_raid_config.py $HOME/artifacts/ip_addrs_100g.txt $HOME/artifacts/raid5_100g.json $chunk 5 $raid_size $num_qp true
-python3 create_raid_config.py $HOME/artifacts/ip_addrs_100g.txt $HOME/artifacts/raid6_100g.json $chunk 6 $raid_size $num_qp true
-python3 create_raid_config.py $HOME/artifacts/ip_addrs_25g.txt $HOME/artifacts/raid5_25g.json $chunk 5 $raid_size $num_qp true
-python3 create_raid_config.py $HOME/artifacts/ip_addrs_25g.txt $HOME/artifacts/raid6_25g.json $chunk 6 $raid_size $num_qp true
+python3 create_raid_config.py $HOME/artifacts/ip_addrs_100g.txt $HOME/artifacts/raid5_100g_d.json $chunk 5 $raid_size $num_qp true
+python3 create_raid_config.py $HOME/artifacts/ip_addrs_100g.txt $HOME/artifacts/raid6_100g_d.json $chunk 6 $raid_size $num_qp true
+python3 create_raid_config.py $HOME/artifacts/ip_addrs_25g.txt $HOME/artifacts/raid5_25g_d.json $chunk 5 $raid_size $num_qp true
+python3 create_raid_config.py $HOME/artifacts/ip_addrs_25g.txt $HOME/artifacts/raid6_25g_d.json $chunk 6 $raid_size $num_qp true
