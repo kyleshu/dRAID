b9e87a3c21037a9c125ec42a30092d471b00cdf4
kyleshu <shujunyi@gmail.com>
2023-01-29 11:01:56 +0800

    Update generate_raid_config.sh

--- FIO/SPDK/generate_raid_config.sh
+++ FIO/SPDK/generate_raid_config.sh
@@ -4,6 +4,8 @@ chunk=$1
 raid_size=$2
 num_qp=$3
 
+cd ..
+
 python3 create_raid_config.py $HOME/artifacts/ip_addrs_100g.txt $HOME/artifacts/raid5_100g.json $chunk 5 $raid_size $num_qp false
 python3 create_raid_config.py $HOME/artifacts/ip_addrs_100g.txt $HOME/artifacts/raid6_100g.json $chunk 6 $raid_size $num_qp false
 python3 create_raid_config.py $HOME/artifacts/ip_addrs_25g.txt $HOME/artifacts/raid5_25g.json $chunk 5 $raid_size $num_qp false
@@ -21,3 +23,5 @@ python3 create_spdk_raid_config.py $HOME/artifacts/ip_addrs_100g.txt $HOME/artif
 python3 create_spdk_raid_config.py $HOME/artifacts/ip_addrs_100g.txt $HOME/artifacts/raid6_spdk_100g_d.json $chunk 6 $raid_size true
 python3 create_spdk_raid_config.py $HOME/artifacts/ip_addrs_25g.txt $HOME/artifacts/raid5_spdk_25g_d.json $chunk 5 $raid_size true
 python3 create_spdk_raid_config.py $HOME/artifacts/ip_addrs_25g.txt $HOME/artifacts/raid6_spdk_25g_d.json $chunk 6 $raid_size true
+
+cd -
