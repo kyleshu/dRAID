2a49474a1c448100315a1d2752ef2e803d5031d5
kyleshu <shujunyi@gmail.com>
2023-01-29 20:18:56 +0800

    draid 9

---
+++ FIO/dRAID/fig9/run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+username=$1
+io_size=$2
+
+if [[ "$io_size" == "4" ]]
+then
+  ../run_server_remote_nvme.sh $username raid5 100g 512 6 8
+  sleep 3
+  sudo LD_PRELOAD=../spdk_bdev /usr/local/bin/fio ../raid5_read.conf -ioengine=../spdk_bdev -iodepth=28 -rw=randread -bs=4k -numjobs=8
+  exit 0
+fi
+
+if [[ "$io_size" == "8" ]]
+then
+  ../run_server_remote_nvme.sh $username raid5 100g 512 6 8
+  sleep 3
+  sudo LD_PRELOAD=../spdk_bdev /usr/local/bin/fio ../raid5_read.conf -ioengine=../spdk_bdev -iodepth=22 -rw=randread -bs=8k -numjobs=8
+  exit 0
+fi
+
+if [[ "$io_size" == "16" ]]
+then
+  ../run_server_remote_nvme.sh $username raid5 100g 512 6 8
+  sleep 3
+  sudo LD_PRELOAD=../spdk_bdev /usr/local/bin/fio ../raid5_read.conf -ioengine=../spdk_bdev -iodepth=12 -rw=randread -bs=16k -numjobs=8
+  exit 0
+fi
+
+if [[ "$io_size" == "32" ]]
+then
+  ../run_server_remote_nvme.sh $username raid5 100g 512 6 2
+  sleep 3
+  sudo LD_PRELOAD=../spdk_bdev /usr/local/bin/fio ../raid5_read.conf -ioengine=../spdk_bdev -iodepth=32 -rw=randread -bs=32k -numjobs=2
+  exit 0
+fi
+
+if [[ "$io_size" == "64" ]]
+then
+  ../run_server_remote_nvme.sh $username raid5 100g 512 6 2
+  sleep 3
+  sudo LD_PRELOAD=../spdk_bdev /usr/local/bin/fio ../raid5_read.conf -ioengine=../spdk_bdev -iodepth=24 -rw=randread -bs=64k -numjobs=2
+  exit 0
+fi
+
+if [[ "$io_size" == "128" ]]
+then
+  ../run_server_remote_nvme.sh $username raid5 100g 512 6 2
+  sleep 3
+  sudo LD_PRELOAD=../spdk_bdev /usr/local/bin/fio ../raid5_read.conf -ioengine=../spdk_bdev -iodepth=16 -rw=randread -bs=128k -numjobs=2
+  exit 0
+fi
+
+echo "We do not have a pre-written command for this I/O size. Feel free to run your own command."
